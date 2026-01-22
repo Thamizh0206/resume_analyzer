096b7d48450bb0731258b799075ee4621662e520
Thamish-02 <thamizhvendhan0206@gmail.com>
2026-01-22 23:40:46 +0530

    created repo

---
+++ backend/api.py
@@ -0,0 +1,14 @@
+import nltk
+
+def setup_nltk():
+    try:
+        nltk.data.find("tokenizers/punkt")
+    except LookupError:
+        nltk.download("punkt")
+
+    try:
+        nltk.data.find("corpora/stopwords")
+    except LookupError:
+        nltk.download("stopwords")
+
+setup_nltk()
